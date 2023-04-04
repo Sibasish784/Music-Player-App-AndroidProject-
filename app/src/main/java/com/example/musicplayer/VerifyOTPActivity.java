ba16779cf3bbd506245aff909b2264b1378271c6
Soumya Ranjan Nayak <soumya95833@gmail.com>
2023-04-04 21:40:38 +0530

    update

---
+++ app/src/main/java/com/example/musicplayer/VerifyOTPActivity.java
@@ -0,0 +1,14 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerifyOTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_otpactivity);
+    }
+}
