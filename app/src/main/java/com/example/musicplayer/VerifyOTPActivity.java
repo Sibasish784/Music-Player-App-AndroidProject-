7ddd98049e1f0c03f49860b58dfb4561b758ef12
Soumya Ranjan Nayak <soumya95833@gmail.com>
2023-04-05 13:29:50 +0530

    update

--- app/src/main/java/com/example/musicplayer/VerifyOTPActivity.java
+++ app/src/main/java/com/example/musicplayer/VerifyOTPActivity.java
@@ -3,12 +3,105 @@ package com.example.musicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class VerifyOTPActivity extends AppCompatActivity {
+
+    private EditText inputCode1, inputCode2, inputCode3, inputCode4, inputCode5, inputCode6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_otpactivity);
+
+        TextView textMobile =findViewById(R.id.textMobile);
+        textMobile.setText(String.format(
+                "+91-%s", getIntent().getStringArrayExtra("mobile")
+        ));
+
+        inputCode1 = findViewById(R.id.inputCode1);
+        inputCode2 = findViewById(R.id.inputCode2);
+        inputCode3 = findViewById(R.id.inputCode3);
+        inputCode4 = findViewById(R.id.inputCode4);
+        inputCode5 = findViewById(R.id.inputCode5);
+        inputCode6 = findViewById(R.id.inputCode6);
+
+        setupOTPInputs();
+
+    }
+
+    private void setupOTPInputs() {
+        inputCode1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().isEmpty()) {
+                    inputCode2.requestFocus();
+                }
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+        inputCode2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().isEmpty()) {
+                    inputCode3.requestFocus();
+                }
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+        inputCode3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().isEmpty()) {
+                    inputCode4.requestFocus();
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+        inputCode4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().isEmpty()) {
+                    inputCode5.requestFocus();
+                }
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+        inputCode5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().isEmpty()) {
+                    inputCode6.requestFocus();
+                }
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
     }
 }
